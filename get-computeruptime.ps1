b8ef31aeecaaeb51ac5b90f4b47e97cbc2bd9dfb
rem-404 <139486652+rem-404@users.noreply.github.com>
2026-05-26 23:15:54 +0800

    Create get-computeruptime.ps1

---
+++ get-computeruptime.ps1
@@ -0,0 +1,41 @@
+function Get-ComputerUptime {
+  [cmdletbinding()]
+  param (
+    [alias('name')]
+    [Parameter(
+      ValueFromPipeline = $true,
+      ValueFromPipelineByPropertyName = $true
+    )]
+    [string[]]$computername = 'localhost',
+    [pscredential]$credential
+  )
+
+  BEGIN {
+    if (-not $credential) {
+      $credential = Get-Credential
+    }
+  }
+
+  PROCESS {
+
+    try {
+      invoke-command -computername $computername -credential $credential -scriptblock {
+        $os = get-ciminstance win32_operatingsystem
+        $uptime = (get-date) - (get-date $os.lastbootuptime)
+        [pscustomobject]@{
+          ComputerName = $env:computername
+          OS           = $os.caption
+          Uptime       = "$($uptime.days) Days, $($uptime.Hours) Hours, $($uptime.Minutes) Minutes"
+        }
+      } -ErrorAction stop
+
+    }
+    catch {
+      Write-Error "Error occurred from $computername : $($_.Exception.Message)"
+    }
+
+  } # PROCESS
+
+  END {}
+
+} # function
